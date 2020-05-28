bdcb422a9b04b64b5b089837c41978201c26aa13
trystep <>
2020-05-28 23:08:17 +0300

    refactoring

--- mysite/app/scripts/sitemap_gen.py
+++ mysite/app/scripts/sitemap_gen.py
@@ -3,9 +3,15 @@ def sitemap_gen(path):
     new_sitemap = open('text.txt', 'w', encoding="utf-8")
 
     for line in new_key:
-        new_url = "<url><loc>http://parf-vlog.ru/?search=" + line.replace("\n",
-                                                                          "") + "</loc><lastmod>2020-05-18</lastmod><changefreq>weekly</changefreq><priority>0.8</priority></url>"
-        new_sitemap.write(new_url + '\n')
+        new_url="""
+    <url>
+        <loc>http://parf-vlog.ru/?search="""+line.replace('\n','')+"""</loc>
+        <lastmod>2020-05-28</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>"""
+
+        new_sitemap.write(new_url)
 
     new_key.close()
     new_sitemap.close()
