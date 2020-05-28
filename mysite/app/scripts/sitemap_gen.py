0783d9b0d21fd8d9f1caeea01b62876c61f8478c
trystep <>
2020-05-28 22:53:25 +0300

    Добавил скрипт для генерации sitemap

---
+++ mysite/app/scripts/sitemap_gen.py
@@ -0,0 +1,14 @@
+def sitemap_gen(path):
+    new_key = open(path, "r", encoding="utf-8")
+    new_sitemap = open('text.txt', 'w', encoding="utf-8")
+
+    for line in new_key:
+        new_url = "<url><loc>http://parf-vlog.ru/?search=" + line.replace("\n",
+                                                                          "") + "</loc><lastmod>2020-05-18</lastmod><changefreq>weekly</changefreq><priority>0.8</priority></url>"
+        new_sitemap.write(new_url + '\n')
+
+    new_key.close()
+    new_sitemap.close()
+
+
+sitemap_gen('new_key.csv')
