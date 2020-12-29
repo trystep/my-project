38265521f36ad645d8fe597f937f7d87a6efb8f0
trystep <alexeevmakc@mail.ru>
2020-12-29 18:06:04 +0300

    refactoring

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -4,69 +4,16 @@ import urllib.parse
 from bs4 import BeautifulSoup
 import requests
 from random import choice
+from .scripts.yttool import detail_view, search_view
 
 
 def search(request):
     """
     Поиск по YouTube на запрос от пользователя сайта
     """
-    keyword = urllib.parse.quote(request.GET.get('search', ''))
-
-    if not keyword:
-        keyword = 'news'
-
-    page = str(urlopen(("https://www.youtube.com/results?search_query=%s&disable_polymer=1" % keyword)).read(),
-               encoding='utf-8')
-    page_source = page.split()
-
-    temp = 0
-
-    link_output = []  # OUTPUT VIDEO LINKS FROM SEARCH
-    title_output = []  # OUTPUT TITLE LINKS FROM SEARCH
-
-    for index in range(0, len(page_source) - 1):
-        element = page_source[index]
-        element_next = page_source[index + 1]
-        if (element[0:23] == 'data-sessionlink="itct=') and (element_next[0:7] == 'title="'):
-            buffer = ""
-            init = page_source[index + 1]
-            buffer += init
-            sub_index = index + 2
-            end = index + 22
-            while sub_index < end:
-                this_element = page_source[sub_index]
-                next_element = page_source[sub_index + 1]
-                if (this_element[len(this_element) - 1]) == '"':
-                    if next_element == 'rel="spf-prefetch"':
-                        buffer += (" " + this_element)
-                        title_output.append(buffer)
-                        break
-                else:
-                    buffer += (" " + this_element)
-                sub_index += 1
-
-    for element in page_source:
-        if element[0:15] == 'href="/watch?v=' and len('www.youtube.com' + element[6:len(element) - 1]) == 35:
-            temp += 1
-            if temp % 2 == 0:
-                link_output += ['www.youtube.com' + element[6:len(element) - 1]]
-
-    data = []
-    try:
-        for index in range(0, len(link_output) - 1):
-            # Replace
-            title_output[index] = title_output[index].replace('&#39;', "")
-            title_output[index] = title_output[index].replace('&quot;', '"')
-
-            link_and_title = [link_output[index], title_output[index][6:]]
-            data.append(link_and_title)
-    except:
-        # print("Ошибка, но работает. Не знаю почему, надо исправить")
-        data.append(link_and_title)
-
-    context = {"search_query": keyword,
-               "data": data,
-               }
+    search_view()
+    data = search_view()
+    context = {"data": data,}
     return render(request, 'base.html', context=context)
 
 
