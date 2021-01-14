62506a6038b1e2ba4f2470b38d8a69214e46db70
trystep <alexeevmakc@mail.ru>
2021-01-14 23:55:41 +0300

    fix sitemap.xml

---
+++ mysite/app/scripts/sitemap_gen.py
@@ -0,0 +1,19 @@
+def sitemap_gen(path):
+    new_key = open(path, "r", encoding="utf-8")
+    new_sitemap = open('new_url_for_sitemap.txt', 'w', encoding="utf-8")
+
+    for line in new_key:
+        new_url = """
+    <url>
+        <loc>https://www.clipic.ru/?search=""" + line.replace('\n', '') + """</loc>
+        <lastmod>2021-01-13</lastmod>
+        <priority>0.8</priority>
+    </url>"""
+
+        new_sitemap.write(new_url)
+
+    new_key.close()
+    new_sitemap.close()
+
+
+sitemap_gen('keywords.csv')
